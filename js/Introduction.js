f909afda21bb5a1cbb1bf22e43ec46f39d3bbc8d
joey155343 <joey155343@hotmail.com>
2020-06-10 22:17:43 +0800

    v2.6 顯示圖片成功 介紹頁籤完成 + 連動

--- js/Introduction.js
+++ js/Introduction.js
@@ -1,50 +1,8 @@
-// 麵包屑
-// $("ul.dropdown-menu li").on("click", function () {
-//     let i = $(this).attr("data-store");
-//     let text = $("a.dropdown-toggle")
-//     switch (i) {
-//         case '餐廳':
-//             text.text("寵物餐廳");
-//             break;
-//         case '旅館':
-//             text.text("寵物旅館");
-//             break;
-//         case '美容':
-//             text.text("寵物美容");
-//             break;
-//         case '學校':
-//             text.text("寵物學校");
-//             break;
-//         case '醫院':
-//             text.text("寵物醫院");
-//             break;
-//     }
-// });
-
-function blobToDataURL(blob, callback) {
-    let a = new FileReader();
-    a.onload = function (e) { callback(e.target.result); }
-    a.readAsDataURL(blob);
-}
-function geturl(blob) {
-    blobToDataURL(blob, function (dataURL) {
-        // console.log(dataURL);
-        return dataURL
-    });
-}
-
-function typedArrayToURL(typedArray, mimeType) {
-    console.log(URL.createObjectURL(new Blob([typedArray.buffer], { type: mimeType })))
-    return URL.createObjectURL(new Blob([typedArray.buffer], { type: mimeType }))
-}
-
-
-var storeVOlist = '';
 
 window.onload = (event) => {
     let githubURL = new URL(window.location.href);
     let params = githubURL.searchParams;
-    console.log(githubURL.searchParams.toString());
+    // console.log(githubURL.searchParams.toString());
     for (let pair of params.entries()) {
         console.log(`key: ${pair[0]}, value: ${pair[1]}`)
 
@@ -93,40 +51,103 @@ window.onload = (event) => {
             },
 
             success: function (data) {
-                console.log(data);
-                storeVOlist = data;
+                // console.log(data);
                 // $("#storelist_card").empty();
                 let cards_html = '';
                 if (data.length != 0) {
+
                     $.each(data, function (index, item) {
-
-
-                        var blob = new Blob([item.store_image1], { type: 'image/jpg' })
-
-                        // ./images/卡片圖.jpg   ${objectURL}
+                        let src = 'https://via.placeholder.com/268x150';
+                        if (item.store_image1 != null) {
+                            let bytes = new Uint8Array(item.store_image1.length);
+                            for (let i = 0; i < bytes.length; i++) {
+                                bytes[i] = item.store_image1[i];
+                            }
+                            let blob = new Blob([bytes], { type: 'image/png' });
+                            src = URL.createObjectURL(blob);
+                        }
                         cards_html += `
-                            <div class="col-12 col-sm-6 col-md-3 d-flex justify-content-center cardview">
-                                <div class="card" style="width: 18rem;">
-                                    <img src="" class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                        <h5 class="card-title">${item.store_name}</h5>
-                                        <p class="card-text">` + item.store_introduction + `</p>
-                                        <div class="d-flex justify-content-end"> 
-                                            <a href="#" class="btn btn-primary">預約</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>`
-
+                          <div class="col-12 col-sm-6 col-md-3 d-flex justify-content-center cardview">
+                          <div class="card" style="width: 18rem;" onclick="pageMiddle(this)" name="${item.store_id}">
+                            <img src="${src}" class="card-img-top" alt="..."></img>
+                                  <div class="card-body">
+                                      <h5 class="card-title">${item.store_name}</h5>
+                                      <p class="card-text">${item.store_introduction}</p>
+                                      <div class="d-flex justify-content-end"> 
+                                          <a href="#" class="btn btn-primary" onclick="pageBooking(this)" name="${item.store_id}">預約</a>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>`;
                     });
                 }
                 $("#storelist_card").prepend(cards_html);
+                // 連動介紹頁籤 - 第一筆
+                reMiddlePage(data[0].store_id);
             }
         });
     }
 };
+// 卡片->介紹頁籤
+function pageMiddle(e) {
+    $("#pills-profile-tab").tab('show');
+    let id = $(e).attr("name");
+    reMiddlePage(id);
+}
+// 預約按鈕->預約頁籤
+function pageBooking(e) {
+    $("#pills-contact-tab").tab('show');
+    event.stopPropagation();
+    // 連動介紹頁籤
+    let id = $(e).attr("name");
+    reMiddlePage(id);
+}
+// 介紹頁籤
+function reMiddlePage(store_id) {
+    console.log(store_id);
+    // location.reload()
+    $.ajax({
+        url: "http://localhost:8081/TDA101G2/Store_frontController",
+        type: "GET",                  // GET | POST | PUT | DELETE | PATCH
+        data: {
+            "action": "getStoreVO",
+            "storeId": store_id
+        },
+        dataType: "json",             // 預期會接收到回傳資料的格式： json | xml | html
+        beforeSend: function () {       // 在 request 發送之前執行
+        },
+        statusCode: {                 // 狀態碼
+            200: function (res) {
+                console.log("200")
+            },
+            404: function (res) {
+                console.log("400")
+            },
+            500: function (res) {
+                console.log("500")
+            }
+        },
+        error: function (xhr) {         // request 發生錯誤的話執行
+            console.log(xhr.responseText);
+        },
 
-
-function aaa() {
-    console.log(storeVOlist[0])
+        success: function (data) {
+            if (data.length != 0) {
+                $("#showStore_introduction").text(data.store_introduction);
+                let imgArray = [data.store_image1, data.store_image2, data.store_image3]
+                for (i = 0; i < 3; i++) {
+                    let src = 'https://via.placeholder.com/1200x800';
+                    if (imgArray[i] != null) {
+                        let bytes = new Uint8Array(imgArray[i].length);
+                        for (let x = 0; x < bytes.length; x++) {
+                            bytes[x] = imgArray[i][x];
+                        }
+                        let blob = new Blob([bytes], { type: 'image/png' });
+                        src = URL.createObjectURL(blob);
+                        $("#showStore_image" + (i + 1)).attr("src", src);
+                    }
+                };
+            }
+        }
+    });
 }
